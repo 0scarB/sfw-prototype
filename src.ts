ed9b38e11bc09a8e34df9ee0f126725d2e8078e7
0scarB <oscarb@protonmail.com>
2023-05-26 02:28:54 +0200

    Add errors as values

--- src.ts
+++ src.ts
@@ -1,3 +1,24 @@
-export const createHelloWorld = (): string => "Hello World!"
+// Errors as values
+// ===========================================================================
 
-export const printHelloWorld = (): void => console.log(createHelloWorld())
+type Ok<T> = {ok: T}
+type Err<Kind extends string = "Unknown"> = {
+    kind: Kind,
+    msg: string,
+}
+type OkOrErr<T, ErrKind extends string = "Unknown"> = Ok<T> | Err<ErrKind>
+
+const ok = <T>(value: T): Ok<T> => ({ok: value})
+
+const err = <Kind extends string = "Unknown">(
+    {kind, msg}: Kind extends "Unknown" ? {kind?: Kind, msg: string} : {kind: Kind, msg: string}
+): Err<Kind> => ({
+    kind: (kind ?? "Unknown") as Kind,
+    msg,
+})
+
+const isOk = <T, ErrKind extends string = "Unknown">(okOrErr: OkOrErr<T, ErrKind>): okOrErr is Ok<T> =>
+    "ok" in okOrErr
+
+const isErr = <T, ErrKind extends string = "Unknown">(okOrErr: OkOrErr<T, ErrKind>): okOrErr is Ok<T> =>
+    "msg" in okOrErr
